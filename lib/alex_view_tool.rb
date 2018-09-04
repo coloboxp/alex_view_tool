1b5bcf1d61ab6f6888e33791a49d0ccc86cf1fd7
Alejandro Alemán <alejandro.aleman@gmail.com>
2018-09-04 15:23:22 +0200

    Dev Tool Initial commit

---
+++ lib/alex_view_tool.rb
@@ -0,0 +1,6 @@
+require "alex_view_tool/version"
+require "alex_view_tool/renderer"
+
+module AlexViewTool
+  # Your code goes here...
+end
---
+++ lib/alex_view_tool/version.rb
@@ -0,0 +1,3 @@
+module AlexViewTool
+  VERSION = "0.1.0"
+end
